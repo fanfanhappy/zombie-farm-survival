d9c70bb81330acea84de24b1df213ab0758b3581
fanfanhappy <fanfanhappy@users.noreply.github.com>
2026-09-08 09:53:28 +0800

    补充资源数据库完整性验收

---
+++ tests/resource_database_test.gd
@@ -0,0 +1,48 @@
+extends SceneTree
+
+
+func _init() -> void:
+	var item_database := load("res://resources/items/item_database.tres") as ItemDatabase
+	var item_catalog := item_database.build_catalog()
+	assert(item_catalog.size() == 22)
+	for item_id in item_catalog:
+		assert(not str(item_id).is_empty())
+		assert((item_catalog[item_id] as Dictionary).get("icon") is Texture2D)
+
+	var recipe_catalog := (load("res://resources/recipes/recipe_database.tres") as RecipeDatabase).build_catalog()
+	assert(recipe_catalog.size() == 11)
+	for recipe in recipe_catalog.values():
+		for item_id in (recipe as Dictionary).get("ingredients", {}): assert(item_catalog.has(item_id))
+		for item_id in (recipe as Dictionary).get("results", {}): assert(item_catalog.has(item_id))
+
+	var crop_catalog := (load("res://resources/crops/crop_database.tres") as CropDatabase).build_catalog()
+	assert(crop_catalog.size() == 3)
+	for crop in crop_catalog.values():
+		var data := crop as Dictionary
+		assert(item_catalog.has(data.get("seed_item")))
+		assert(data.get("visual_scene") is PackedScene)
+		for item_id in data.get("harvest", {}): assert(item_catalog.has(item_id))
+
+	var placement_catalog := (load("res://resources/placements/placeable_database.tres") as PlaceableDatabase).build_catalog()
+	assert(placement_catalog.size() == 4)
+	for definition in placement_catalog.values(): assert((definition as Dictionary).get("scene") is PackedScene)
+
+	var weather_catalog := (load("res://resources/weather/weather_database.tres") as WeatherDatabase).build_catalog()
+	assert(weather_catalog.size() == 3)
+	var total_weather_weight := 0
+	for weather in weather_catalog.values(): total_weather_weight += int((weather as Dictionary).get("weight", 0))
+	assert(total_weather_weight > 0)
+
+	var objectives := (load("res://resources/objectives/objective_database.tres") as ObjectiveDatabase).build_list()
+	assert(objectives.size() == 7)
+	for objective in objectives:
+		for item_id in (objective as Dictionary).get("reward", {}): assert(item_catalog.has(item_id))
+
+	var horde := load("res://resources/hordes/first_horde.tres") as HordeDefinition
+	assert(horde.waves.size() == 3)
+	for item_id in horde.reward: assert(item_catalog.has(item_id))
+
+	var upgrades := (load("res://resources/defense_upgrades/defense_upgrade_database.tres") as DefenseUpgradeDatabase).build_catalog()
+	assert(upgrades.size() == 2)
+	print("RESOURCE_DATABASE_INTEGRITY_OK")
+	quit()
